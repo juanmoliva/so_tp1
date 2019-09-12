f68277124f00f7cad09ea4f93c593c2bcdbc1ebb
JuanOliva98 <42985576+JuanOliva98@users.noreply.github.com>
2019-09-12 19:34:53 -0300

    added execvp and fifos between parent and childs

--- slave.c
+++ slave.c
@@ -1,10 +1,27 @@
- #include <sys/types.h>
- #include <unistd.h>
- #include <stdio.h>
- #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
-	
+	// argv[1] should be an int
+    int initial_files = atoi(argv[1]);
 
+    printf("hello from slave , %d files to receive.\n", initial_files );
+
+    char fifo_path[32];
+    sprintf(fifo_path, "/tmp/fifo-%d", getpid());
+
+    int fd  = open(fifo_path, O_RDONLY);
+    char buf[1024];
+
+    int n = read(fd, buf, sizeof(buf));
+
+    printf("message received from pid %d : %s\n", getpid(), buf );
+    close(fd);
+
+    return 0;
 }
--- solve.c
+++ solve.c
@@ -1,25 +1,29 @@
- #include <sys/types.h>
- #include <unistd.h>
- #include <stdio.h>
- #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #define NUM_SLAVES 5
 #define MAX_FILES 200
+#define INITIAL_FILES_FOR_SLAVE "2"
 
 int main(int argc, char *argv[])
 {
     // accept dir/* , dir/file.cnf , file.cnf as parameters
 
-    if ( argc < 2 ) {
+    /*if ( argc < 2 ) {
         printf("usage: solve [FILES] \n");
         printf("files must be .cnf\n");
-        return;
-    }
+        return 1;
+    }*/
 
+    /*
     char *to_solve[MAX_FILES];
     for( int i = 1 ; i < argc ; i++ ) {
         char *to_solve[i-1] = argv[i];
     }
-
+    */
 
 	// create NUM_SLAVES slaves
 	pid_t pid;
@@ -32,11 +36,41 @@ int main(int argc, char *argv[])
         pid = fork();
     } 
 
+    int fd_fifos[NUM_SLAVES];
+
     if ( pid != 0 ) {
     	// padre
         slaves_pids[NUM_SLAVES-1] = pid; 
+
+        // make fifos
+        for(int i = 0; i < NUM_SLAVES ; i++) {
+            char* buffs_1 = "messsage_1";
+            char fifo_path[32];
+            sprintf(fifo_path, "/tmp/fifo-%d", slaves_pids[i]);
+            mkfifo(fifo_path, 0666);
+            fd_fifos[i] = open(fifo_path, O_WRONLY);
+            write(fd_fifos[i], buffs_1, sizeof(buffs_1));
+            close(fd_fifos[i]);
+        }
+
     }
 
+    if ( pid == 0 ){
+        // a slave
+        char *args_slave[]={"./slave",INITIAL_FILES_FOR_SLAVE, NULL}; 
+        printf("exec executed.\n");
+        execvp(args_slave[0],args_slave); 
+        printf("problem with exec\n");
+        return 2;
+    }
+
+    char* buffs_1 = "messsage 1";
+    char* buffs_2 = "messsage 2";
+
 	// distribute initial files to slaves
+
+
+
+    return 0;
 
 }
